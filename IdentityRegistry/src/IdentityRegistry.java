7ebe7083ef23ad63fd0f7b60748de570fa66a56c
HaJaehee <lovesm135@naver.com>
2017-02-14 12:52:34 +0900

    API methods integreted to setPort()
    setMIR(), setMSR(), setMSP() are integrated to setPort()

--- IdentityRegistry/src/IdentityRegistry.java
+++ IdentityRegistry/src/IdentityRegistry.java
@@ -24,7 +24,7 @@ public class IdentityRegistry{
 		MMSConfiguration.MMS_URL="127.0.0.1:8088";
 		
 		MMSClientHandler ch = new MMSClientHandler(myMRN);
-		ch.setMIR(port);
+		ch.setPort(port);
 		
 		//Request Callback from the request message
 		ch.setCallback(new MMSClientHandler.Callback() {
