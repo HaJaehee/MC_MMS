f6f1eef28d52c6a620e96f53ab4a715b33776619
HaJaehee <lovesm135@naver.com>
2017-03-20 06:48:30 +0900

    https working
    https working but the browser notifying unsafety

--- MMSServer/src/kr/ac/kaist/mms_server/MMSServer.java
+++ MMSServer/src/kr/ac/kaist/mms_server/MMSServer.java
@@ -3,7 +3,7 @@ package kr.ac.kaist.mms_server;
 /* -------------------------------------------------------- */
 /** 
 File name : MMSServer.java
-	Executable class 
+	It is executable class of MMS Server.
 Author : Jaehee Ha (jaehee.ha@kaist.ac.kr)
 	Haeun Kim (hukim@kaist.ac.kr)
 	Jaehyun Park (jae519@kaist.ac.kr)
@@ -34,16 +34,6 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.haproxy.HAProxyProxiedProtocol.TransportProtocol;
 
-/* -------------------------------------------------------- */
-/** 
-File name : MMSServer.java
-	It is executable class of MMS Server.
-Author : Jaehyun Park (jae519@kaist.ac.kr)
-Creation Date : 2017-01-24
-Version : 0.3.01
-*/
-/* -------------------------------------------------------- */
-
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.ssl.SslHandler;
@@ -55,34 +45,10 @@ public class MMSServer {
 	private static final String TAG = "MMSServer";
 	
 	public static void main(String[] args) throws Exception{
-		
-	    
-	    TrustManagerFactory tmFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-	    
-	    SSLContext serverContext = SSLContext.getInstance("TLS"); //from JDK 7 supports
-	    final KeyStore ks = KeyStore.getInstance("JKS");
-	    tmFactory.init(ks);
-	    
-	    String pass = "lovesm13";
-	    ks.load(new FileInputStream(System.getProperty("user.dir")+"/mmskeystore.jks"), pass.toCharArray());
-	    final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-	    kmf.init(ks, pass.toCharArray());
-	    
-	    KeyManager[] km = kmf.getKeyManagers();
-	    javax.net.ssl.TrustManager[] tm = tmFactory.getTrustManagers();
-	    
-	    serverContext.init(km, tm , null);
-	    
-	    //SslHandler sslHandler = new SslHandler(serverContext.createSSLEngine());
-	    SSLEngine sslengine = serverContext.createSSLEngine();
-	    sslengine.setUseClientMode(false);
-	    sslengine.setEnableSessionCreation(true);
-	    sslengine.setEnabledProtocols(sslengine.getSupportedProtocols());
-	    sslengine.setEnabledCipherSuites(sslengine.getSupportedCipherSuites());
-	    
+
 		if(MMSConfiguration.LOGGING)System.out.println("[MMS Server] Now starting MMS HTTP server");
 		NettyStartupUtil.runServer(MMSConfiguration.HTTP_PORT, pipeline -> {   //runServer(int port, Consumer<ChannelPipeline> initializer)
-			pipeline.addLast("ofchannelcrypto", new SslHandler(sslengine));
+			//pipeline.addLast("ssl", new SslHandler(sslengine));
 			pipeline.addLast(new HttpServerCodec());
             pipeline.addLast(new HttpObjectAggregator(19999));
             pipeline.addLast(new MRH_MessageInputChannel());
---
+++ MMSServer/src/kr/ac/kaist/mms_server/SecureMMSServer.java
@@ -0,0 +1,106 @@
+package kr.ac.kaist.mms_server;
+
+/* -------------------------------------------------------- */
+/** 
+File name : SecureMMSServer.java
+	It is executable class of MMS Secure Server.
+Author : Jaehee Ha (jaehee.ha@kaist.ac.kr)
+Creation Date : 2017-03-20
+Version : 0.4.0
+*/
+/* -------------------------------------------------------- */
+
+
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.TrustManagerFactory;
+
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+
+public final class SecureMMSServer {
+
+    static final int PORT = Integer.parseInt(System.getProperty("port", "443"));
+
+    public static void main(String[] args) throws Exception {
+        
+	    //for information, not working
+	    /*
+	    SSLContext serverContext = SSLContext.getInstance("TLS"); //from JDK 7 supports
+	    final KeyStore ks = KeyStore.getInstance("JKS");
+	    
+	    String pass = "lovesm13";
+	    ks.load(new FileInputStream(System.getProperty("user.dir")+"/mmskeystore.jks"), pass.toCharArray());
+	    final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+	    kmf.init(ks, pass.toCharArray());
+	    
+	    KeyManager[] km = kmf.getKeyManagers();
+	    
+	    serverContext.init(km, null , null);
+	    
+	    SslHandler sslHandler = new SslHandler(serverContext.createSSLEngine());
+	    SSLEngine sslengine = serverContext.createSSLEngine();
+	    sslengine.setUseClientMode(false);
+	    sslengine.setEnableSessionCreation(true);
+	    sslengine.setEnabledProtocols(sslengine.getSupportedProtocols());
+	    sslengine.setEnabledCipherSuites(sslengine.getSupportedCipherSuites());*/
+    	
+	    final KeyStore ks = KeyStore.getInstance("JKS");
+	    
+	    String pass = "lovesm13";
+	    
+	    ks.load(new FileInputStream(System.getProperty("user.dir")+"/mmskeystore.jks"), pass.toCharArray());
+	    final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+	    kmf.init(ks, pass.toCharArray());
+	    KeyManager[] km = kmf.getKeyManagers();
+	
+        //SelfSignedCertificate ssc = new SelfSignedCertificate();
+        //SslContext sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())
+        //    .build();
+        SslContext sslCtx = SslContextBuilder.forServer(kmf).build();
+        
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+             .channel(NioServerSocketChannel.class)
+             .handler(new LoggingHandler(LogLevel.INFO))
+             .childHandler(new SecureMMSServerInitializer(sslCtx));
+
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
