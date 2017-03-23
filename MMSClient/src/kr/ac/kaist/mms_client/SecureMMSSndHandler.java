7e6b16e11110e6ea15f4016f6130881ed01bd406
HaJaehee <lovesm135@naver.com>
2017-03-23 17:03:24 +0900

    passed MMS dual mode test
    passed MMS dual mode test

--- MMSClient/src/kr/ac/kaist/mms_client/SecureMMSSndHandler.java
+++ MMSClient/src/kr/ac/kaist/mms_client/SecureMMSSndHandler.java
@@ -27,18 +27,6 @@ import javax.net.ssl.TrustManager;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-
 import java.io.*;
 import java.net.*;
 import javax.net.ssl.*;
