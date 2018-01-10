95e8808d6692d9a5be6674bce4ccfa40a6497f35
J.Ha <lovesm135@naver.com>
2018-01-10 14:38:46 +0900

    Updated certificates
    Updated certificates

--- MMSKeystoreCoder/src/KeystoreCoder.java
+++ MMSKeystoreCoder/src/KeystoreCoder.java
@@ -7,12 +7,17 @@ Version : 0.5.0
 
 Rev. history : 2017-04-29
 Version : 0.5.3
-	Added system log features
+	Added system log features.
 Modifier : Jaehee Ha (jaehee.ha@kaist.ac.kr)
 
 Rev. history : 2017-06-19
 Version : 0.5.7
-	Applied LogBack framework in order to log events
+	Applied LogBack framework in order to log events.
+Modifier : Jaehee Ha (jaehee.ha@kaist.ac.kr)
+
+Rev. history : 2018-01-10
+Version : 0.7.1
+	Replaced from mykeystore.jks to cert.jks.
 Modifier : Jaehee Ha (jaehee.ha@kaist.ac.kr)
 */
 /* -------------------------------------------------------- */
@@ -27,7 +32,7 @@ public class KeystoreCoder {
 	//private static final Logger logger = LoggerFactory.getLogger(KeystoreCoder.class);
 
 	public static void main(String[] args) {
-		 final File original = new File(System.getProperty("user.dir")+"/mykeystore.jks");
+		 final File original = new File(System.getProperty("user.dir")+"/cert.jks");
 		    
 		 try {
 			System.out.println(Base64Coder.encode(OneUtilsJre.toByteArray(new FileInputStream(original))));
