de58e5e9221bfca18d480e436fdb3f231e6acbb8
J.Ha <jaehee.ha@kaist.ac.kr>
2019-05-20 16:01:17 +0900

    Updated batch script files and shell script files.
    Updated batch script files and shell script files.

--- MMSServer/Windows/start_mns.bat
+++ MMSServer/Windows/start_mns.bat
@@ -1,2 +1,2 @@
 cd ../target
-cmd.exe /c start java -cp ./MC_MMS-0.8.0-SNAPSHOT.jar kr.ac.kaist.mns_interaction.MNSDummy
+cmd.exe /c start java -cp ./MC_MMS-0.9.1-SNAPSHOT.jar kr.ac.kaist.mns_interaction.MNSDummy
