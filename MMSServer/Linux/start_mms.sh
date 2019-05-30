2426bc148d5a0fc3842554f9c1258fa670e31ac7
J.Ha <jaehee.ha@kaist.ac.kr>
2019-05-30 20:32:26 +0900

    Updated version number.
    Updated version number.

--- MMSServer/Linux/start_mms.sh
+++ MMSServer/Linux/start_mms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../target
-java -cp ../target/MC_MMS-0.9.1-SNAPSHOT.jar kr.ac.kaist.mms_server.MMSServer -mqmngproto https
+java -cp ../target/MC_MMS-0.9.2-SNAPSHOT.jar kr.ac.kaist.mms_server.MMSServer -mqmngproto https
