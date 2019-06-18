07d92620f8a0104a43b29bf31ea1dbe62ad2a0ef
J.Ha <jaehee.ha@kaist.ac.kr>
2019-06-18 15:04:05 +0900

    Update start_mms.sh

--- docker/MMSServer/start_mms.sh
+++ docker/MMSServer/start_mms.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -f "/var/mms/logs/mms-server.html" ]; then	
-	now=$(date +"%T")
-	mv /var/mms/logs/mms-server.html /var/mms/logs/mms-server$now.html
+	now=$(date +"%Y-%m-%d.%T")
+	mv /var/mms/logs/mms-server.html /var/mms/logs/mms-server.$now.html
 fi
 java -cp /app/mms/MMSServer.jar kr.ac.kaist.mms_server.MMSServer 
