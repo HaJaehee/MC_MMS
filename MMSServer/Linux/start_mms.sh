8ed13b3295063ccc7095c8f31ea52a0162a6860d
J.Ha <jaehee.ha@kaist.ac.kr>
2019-06-18 15:03:05 +0900

    Update start_mms.sh

--- MMSServer/Linux/start_mms.sh
+++ MMSServer/Linux/start_mms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f "/var/mms/logs/mms-server.html" ]; then	
-	now=$(date +"%T")
-	mv /var/mms/logs/mms-server.html /var/mms/logs/mms-server$now.html
+	now=$(date +"%Y-%m-%d.%T")
+	mv /var/mms/logs/mms-server.html /var/mms/logs/mms-server.$now.html
 fi
 cd ../target
 java -cp ../target/MC_MMS-0.9.3-SNAPSHOT.jar kr.ac.kaist.mms_server.MMSServer -mqmngproto https
