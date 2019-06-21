4d9f5fe3d3ee4f20c9e3a42a9d6ae9ed6c4e8fe5
J.Ha <jaehee.ha@kaist.ac.kr>
2019-06-21 12:51:19 +0900

    Updated Travis CI.

--- MMSServer/Linux/build_mms.sh
+++ MMSServer/Linux/build_mms.sh
@@ -2,7 +2,7 @@
 
 cd ..
 mvn clean install 
-#cp External\ Jars/pkilib-1.4.jar $HOME/.m2/repository/net/etri/net.etri.pkilib/1.4/net.etri.pkilib-1.4.jar
-#mvn install
+cp External\ Jars/pkilib-1.4.jar $HOME/.m2/repository/net/etri/net.etri.pkilib/1.4/net.etri.pkilib-1.4.jar
+mvn install
 cp -r MMS-configuration ./target/
 
