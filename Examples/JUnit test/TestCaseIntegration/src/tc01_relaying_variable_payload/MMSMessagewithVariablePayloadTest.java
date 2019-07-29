586c3b150d8fc446e1057627c58b362f667f566d
J.Ha <jaehee.ha@kaist.ac.kr>
2019-07-29 12:01:52 +0900

    Adjusted timeout of MMS client.

--- Examples/JUnit test/TestCaseIntegration/src/tc01_relaying_variable_payload/MMSMessagewithVariablePayloadTest.java
+++ Examples/JUnit test/TestCaseIntegration/src/tc01_relaying_variable_payload/MMSMessagewithVariablePayloadTest.java
@@ -47,9 +47,9 @@ public class MMSMessagewithVariablePayloadTest extends MMSTestBase {
 		server.terminateServer();
 	}
 	
-	public int sendContentLength(String FileName, int actual_content_length) throws IOException {
+	public int sendContentLength(String FileName, int actual_content_length, int timeout) throws IOException {
 		
-		client.sendContentLength("files/"+FileName,actual_content_length);
+		client.sendContentLength("files/"+FileName,actual_content_length, timeout);
 		int expected_content_length = MMSMessagewithVariablePayloadServer.getContentLength();
 		return expected_content_length;
 	}
@@ -59,7 +59,7 @@ public class MMSMessagewithVariablePayloadTest extends MMSTestBase {
 		int expected_content_length=0;
 		int actual_content_length=0;	
 				
-		expected_content_length = sendContentLength("file0B.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file0B.txt",actual_content_length, 1000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	
@@ -67,77 +67,77 @@ public class MMSMessagewithVariablePayloadTest extends MMSTestBase {
 	public void test02() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=170;		
-		expected_content_length = sendContentLength("file170B.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file170B.txt",actual_content_length, 1000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test03() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=3*1024;		
-		expected_content_length = sendContentLength("file3KB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file3KB.txt",actual_content_length, 1000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test04() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=200*1024;		
-		expected_content_length = sendContentLength("file200KB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file200KB.txt",actual_content_length, 1000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test05() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=500*1024;		
-		expected_content_length = sendContentLength("file500KB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file500KB.txt",actual_content_length, 1000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test06() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=2*1024*1024;		
-		expected_content_length = sendContentLength("file2MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file2MB.txt",actual_content_length, 3000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test07() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=7*1024*1024;		
-		expected_content_length = sendContentLength("file7MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file7MB.txt",actual_content_length, 6000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test08() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=10*1024*1024;		
-		expected_content_length = sendContentLength("file10MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file10MB.txt",actual_content_length, 10000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test09() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=20*1024*1024;		
-		expected_content_length = sendContentLength("file20MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file20MB.txt",actual_content_length, 20000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test10() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=30*1024*1024;		
-		expected_content_length = sendContentLength("file30MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file30MB.txt",actual_content_length, 30000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test11() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=40*1024*1024;		
-		expected_content_length = sendContentLength("file40MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file40MB.txt",actual_content_length, 40000);	
 		assertEquals(expected_content_length,actual_content_length);		
 	}
 	@Test
 	public void test12() throws IOException {
 		int expected_content_length=0;
 		int actual_content_length=50*1024*1024;		
-		expected_content_length = sendContentLength("file50MB.txt",actual_content_length);	
+		expected_content_length = sendContentLength("file50MB.txt",actual_content_length, 30000);	
 		assertTrue(client.getResponse()==413);		
 		
 	}
