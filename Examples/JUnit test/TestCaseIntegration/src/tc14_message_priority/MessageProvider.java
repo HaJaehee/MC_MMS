10fc0ee8e5cd5bcf175e4bde04a6f5fd85fd914b
Jin Jeong <jungst0001@gmail.com>
2019-09-17 13:57:31 +0900

    implemented test code to check sorting function based on the priority

---
+++ Examples/JUnit test/TestCaseIntegration/src/tc14_message_priority/MessageProvider.java
@@ -0,0 +1,76 @@
+package tc14_message_priority;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import kr.ac.kaist.mms_client.MMSClientHandler;
+
+/**
+ * File name : MessageProvider.java
+ * Author : Jin Jeong (jungst0001@kaist.ac.kr) 
+ * Creation Date : 2019-09-17
+ */
+
+public class MessageProvider {
+	private int response = 0;
+	private String myMRN = "urn:mrn:imo:imo-no:ts-mms-14-server";
+//	private String dstMRN = "urn:mrn:imo:imo-no:ts-mms-03-client";
+	private String dstMRN = "urn:mrn:mcl:vessel:dma:poul-lowenorn";
+	private MMSClientHandler myHandler = null;
+
+	public MessageProvider() {
+
+		try {
+			myHandler = new MMSClientHandler(myMRN);
+
+			myHandler.setSender(new MMSClientHandler.ResponseCallback() {
+
+				@Override
+				public void callbackMethod(Map<String, List<String>> headerField, String message) {
+					// TODO Auto-generated method stub
+					if (headerField.get("Response-code") != null) {
+						int code = Integer.parseInt(headerField.get("Response-code").get(0));
+						response = code;
+						System.out.println("Response : " +response);
+					}
+				}
+
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public int getResponse() {
+		return response;
+	}
+	
+	public void sendContent(String priority, String payload) throws IOException {
+		Map<String, List<String>> headerfield = new HashMap<String, List<String>>();
+		
+		List<String> valueList = new ArrayList<String>();
+		valueList.add(priority);
+		headerfield.put("priority", valueList);
+
+		try {
+			myHandler.setMsgHeader(headerfield);
+			myHandler.sendPostMsg(dstMRN, payload, 10000);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void terminateServer() {
+		myHandler.terminateServer();
+	}
+}
---
+++ Examples/JUnit test/TestCaseIntegration/src/tc14_message_priority/PriorityTest.java
@@ -0,0 +1,69 @@
+package tc14_message_priority;
+import static org.junit.Assert.*;
+
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import tc_base.MMSTestBase;
+
+/**
+ * File name : PriorityTest.java
+ * Author : Jin Jeong (jungst0001@kaist.ac.kr) 
+ * Creation Date : 2019-09-17
+ */
+
+@FixMethodOrder(MethodSorters.DEFAULT)
+public class PriorityTest extends MMSTestBase {
+	static PollingClient client;
+	static MessageProvider server;
+	static int offset;
+	
+	@BeforeClass 
+	public static void setupForClass() throws Exception {
+		client = new PollingClient();
+		server = new MessageProvider();
+		offset = 4;
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		server.terminateServer();
+	}
+	
+	@Test
+	public void test01() throws IOException, InterruptedException {		
+		int expected = 0;
+		int actual = 20*1024*1024;	
+		
+		String[] message1 = new String[2];
+		message1[0] = "0";
+		message1[1] = "1";
+		String[] message2 = new String[2];
+		message2[0] = "3";
+		message2[1] = "2";
+		String[] message3 = new String[2];
+		message3[0] = "5";
+		message3[1] = "3";
+				
+		server.sendContent(message1[0], message1[1]);	
+		Thread.sleep(1000);	
+		server.sendContent(message2[0], message2[1]);	
+		Thread.sleep(1000);	
+		server.sendContent(message3[0], message3[1]);	
+		Thread.sleep(1000);	
+		
+		expected = client.pollingReqeust();	
+		Thread.sleep(1000);
+//		assertTrue(server.getResponse() == 200);		
+		System.out.println(client.getPayload());
+		assertTrue(expected==(actual*2)+offset);		
+	}	
+
+}
