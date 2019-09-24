c9116e540c77330703c912098ad04e64babbadba
J.Ha <jaehee.ha@kaist.ac.kr>
2019-09-24 16:45:23 +0900

    Updated version numbers.
    Updated version numbers

--- MMSServer/Windows/start_mms.bat
+++ MMSServer/Windows/start_mms.bat
@@ -1,2 +1,2 @@
 cd ../target
-cmd.exe /c start java -cp ./MC_MMS-0.9.4-SNAPSHOT.jar kr.ac.kaist.mms_server.MMSServer
+cmd.exe /c start java -cp ./MC_MMS-0.9.5-SNAPSHOT.jar kr.ac.kaist.mms_server.MMSServer
