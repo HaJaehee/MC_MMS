693bef44fc62ed2b169ade06647e9d6a00274d7a
J.Ha <jaehee.ha@kaist.ac.kr>
2019-09-26 17:28:21 +0900

    Update MessageLimitSizeDequeuer.java

--- MMSServer/src/main/java/kr/ac/kaist/message_queue/v2/MessageLimitSizeDequeuer.java
+++ MMSServer/src/main/java/kr/ac/kaist/message_queue/v2/MessageLimitSizeDequeuer.java
@@ -457,8 +457,8 @@ public class MessageLimitSizeDequeuer extends MessageQueueDequeuer {
 					@Override
 					public void terminate(ChannelHandlerContext ctx) {
 
-						Integer duplicateInfoCnt = SeamlessRoamingHandler.getDuplicationInfoCnt(duplicationId);
-						if (duplicateInfoCnt != null) {
+						int duplicateInfoCnt = SeamlessRoamingHandler.getDuplicationInfoCnt(duplicationId);
+						if (duplicateInfoCnt != 0) {
 							SeamlessRoamingHandler.releaseDuplicationInfo(duplicationId);
 						}
 						if (bean != null && bean.refCnt() > 0) {
